6bff398998e8b3c07efadbbc98d219f8a751d89f
RENATA AMANCIO <renatasilv221@gmail.com>
2025-02-26 14:56:44 -0300

    upload da primeira aula c#

---
+++ Aula 1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Aula_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 39;
+            string nomecompleto = "Diego Aquila Almeida Sampaio";
+
+            var VariavelSemTipo = "Diego";
+
+
+            Console.WriteLine(VariavelSemTipo.GetType());
+
+            Console.WriteLine("digite o seu nome: ");
+
+            string nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine("digite sua idade; ");
+
+            string idadeUsuario = Console.ReadLine();
+            Console.WriteLine($"meu nome é{nomeUsuario} e a minha idade é {idadeUsuario} ");
+
+
+        }
+           
+    }
+        
